59c2b5e5d6524fbe4aa11dc134d05bea6a18fd15
Shao-Kan Chu <Shao-Kan.Chu@ibm.com>
2025-04-30 10:15:29 -0400

    add new file

---
+++ src/main/java/com/acme/modres/mbean/DMBeanUtils2.java
@@ -0,0 +1,80 @@
+package com.acme.modres.mbean;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.management.MBeanOperationInfo;
+
+public final class DMBeanUtils2 {
+  private static final Logger logger = Logger.getLogger(DMBeanUtils2.class.getName());
+
+  public static MBeanOperationInfo[] getOps(OpMetadataList opList) {
+    MBeanOperationInfo[] ops = null;
+    if (opList == null || opList.getOpMetadatList() == null) {
+      logger.log(Level.WARNING, "No operation is configured");
+      return ops;
+    }
+
+    int numOps = opList.getOpMetadatList().size();
+    if (numOps > 0) {
+      ops = new MBeanOperationInfo[numOps];
+      int i = 0;
+      for (OpMetadata opMetadata : opList.getOpMetadatList()) {
+        String name = opMetadata.getName();
+        String desc = opMetadata.getDescription();
+        String type = opMetadata.getType();
+        int impact = opMetadata.getImpact();
+
+        try {
+          MBeanOperationInfo opInfo = new MBeanOperationInfo(name, desc, /* signature */ null, type, impact, /*
+                                                                                                            * descriptor
+                                                                                                            */ null);
+          ops[i++] = opInfo;
+        } catch (IllegalArgumentException e) {
+          logger.log(Level.WARNING, "Impact level specified is not valid. Using impact level UNKNOWN as the value.");
+          MBeanOperationInfo opInfo = new MBeanOperationInfo(name, desc, /* signature */ null, type, MBeanOperationInfo.UNKNOWN, /*
+                                                                                                                                    * descriptor
+                                                                                                                                    */ null);
+          ops[i++] = opInfo;
+        }
+      }
+    }
+
+    return ops;
+  }
+
+  public static MBeanOperationInfo[] getOps2(OpMetadataList opList) {
+    MBeanOperationInfo[] ops = null;
+    if (opList == null || opList.getOpMetadatList() == null) {
+      logger.log(Level.WARNING, "No operation is configured");
+      return ops;
+    }
+
+    int numOps = opList.getOpMetadatList().size();
+    if (numOps > 0) {
+      ops = new MBeanOperationInfo[numOps];
+      int i = 0;
+      for (OpMetadata opMetadata : opList.getOpMetadatList()) {
+        String name = opMetadata.getName();
+        String desc = opMetadata.getDescription();
+        String type = opMetadata.getType();
+        int impact = opMetadata.getImpact();
+
+        try {
+          MBeanOperationInfo opInfo2 = new MBeanOperationInfo(name, desc, /* signature */ null, type, impact, /*
+          * descriptor
+          */ null);
+          ops[i++] = opInfo2;
+        } catch (IllegalArgumentException e) {
+          logger.log(Level.WARNING, "Impact level specified is not valid. Using MBeanOperationInfo.UNKNOWN as the value for the impact field.");
+          MBeanOperationInfo opInfo2 = new MBeanOperationInfo(name, desc, /* signature */ null, type, MBeanOperationInfo.UNKNOWN, /*
+          * descriptor
+          */ null);
+          ops[i++] = opInfo2;
+        }
+      }
+    }
+
+    return ops;
+  }
+}
